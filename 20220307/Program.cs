458b9b7d32beb39aa189e65dcac0aa6d6de8be77
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:00:17 +0800

    finish

--- 20220307/Program.cs
+++ 20220307/Program.cs
@@ -9,8 +9,10 @@ namespace _20220307
             double meal_cost = 100;
             int tip_percent = 15;
             int tax_percent = 8;
-
-
+            int a = (int)meal_cost;
+            int b = a + tax_percent + tip_percent;
+            Console.WriteLine(b);
         }
     }
 }
+
